72a980fdcaa2f6ddfa230e392b38f04c0246fe54
arcanlte <pinangayjoel@gmail.com>
2020-03-09 17:27:45 +0800

    authServer.js created and initiated.

--- jwt-practice/authServer.js
+++ jwt-practice/authServer.js
@@ -7,22 +7,14 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-const posts = [
-  {
-    username: "Mel",
-    title: "post1"
-  },
-  {
-    username: "Joe",
-    title: "post2"
-  }
-];
+app.post(`/login`, (req, res) => {
+  //Authenitcation user
 
-app.get(`/posts`, authenticateToken, (req, res) => {
-  res.json(posts.filter(post => post.username === req.user.name));
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
 });
-
-
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
@@ -36,4 +28,5 @@ function authenticateToken(req, res, next) {
   });
 }
 
-app.listen(3000);
+app.listen(4000);
+ 
