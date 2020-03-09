fdc1115caaf4fc4934860bec0f734f65924e985e
arcanlte <pinangayjoel@gmail.com>
2020-03-09 17:10:08 +0800

    JWT is working

--- jwt-practice/server.js
+++ jwt-practice/server.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const posts = [
   {
@@ -12,8 +18,29 @@ const posts = [
   }
 ];
 
-app.get(`/posts`, (req, res) => {
-  res.json(posts);
+app.get(`/posts`, authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
 });
+
+app.post(`/login`, (req, res) => {
+  //Authenitcation user
+
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
