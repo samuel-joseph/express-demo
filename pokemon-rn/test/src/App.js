89f8ea65d38e5de20d81b1221b2304b2507aae5a
arcanlte <pinangayjoel@gmail.com>
2020-03-15 17:05:58 +0800

    Boiler plate created for app.js  file

--- pokemon-rn/test/src/App.js
+++ pokemon-rn/test/src/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
-const api = async () => {
-  console.log("WHAAAAT");
-  const resp = await axios.get("http://localhost:3001/pokemons/pokedex");
-  console.log(resp);
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <button onClick={api}>HELLO</button>
-    </div>
-  );
+    this.state = {
+      
+    };
+  }
+
+  componentDidMount = async () => {
+    const resp = await axios.get("http://localhost:3001/pokemons/pokedex");
+    console.log(resp);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button>HELLO</button>
+      </div>
+    );
+  }
 }
-
-export default App;
