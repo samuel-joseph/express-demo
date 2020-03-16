b918ea6f904195ce3a155685718a551790739d02
arcanlte <pinangayjoel@gmail.com>
2020-03-16 11:34:07 +0800

    map is working.

--- pokemon-rn/test/src/App.js
+++ pokemon-rn/test/src/App.js
@@ -7,19 +7,38 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      
+      pokemons: [],
+      cool: ["a", "b", "c"]
     };
   }
 
   componentDidMount = async () => {
     const resp = await axios.get("http://localhost:3001/pokemons/pokedex");
-    console.log(resp);
+    const pokemons = resp.data.pokemons;
+
+    this.setState({
+      pokemons
+    });
+    console.log(this.state.pokemons);
   };
 
   render() {
     return (
       <div className="App">
-        <button>HELLO</button>
+        {this.state.pokemons && (
+          <>
+            {console.log("HEYOOO")}
+            {console.log(this.state)}
+            {this.state.pokemons.map(data => (
+              <div className="inner">
+                <div className="desc">
+                  <h4>{data.name}</h4>
+                  <img src={data.frontImage} />
+                </div>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     );
   }
